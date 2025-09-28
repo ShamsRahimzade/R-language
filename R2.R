9fb8086724e8e912a6bbdb894a2f8884588d0567
ShamsRahimzade <tu7mbmrvb@code.edu.az>
2025-09-28 17:47:31 +0400

    cbind,rbind,merge

--- R2.R
+++ R2.R
@@ -9,3 +9,24 @@ cbind_data<-cbind(human[2],hobby[4])
 cbind_data2<-cbind(human$Cinsiyet,hobby$Yemek)
 names(cbind_data2)<-c("Gender","Food")
 
+rbind_data<-rbind(human$`Ad-Soyad`,hobby$Fobi)
+rbind_data2<-rbind(human[1],hobby[2])  
+
+
+
+install.packages("writexl")
+library(dplyr)
+library(writexl)
+bind_data<-bind_rows(human,hobby)
+
+write.csv(human,"human.csv")
+write.table(human,"human.txt",row.names = F,sep = ",")
+write_xlsx(human,"human.xlsx")
+
+# df1 <- human["Cinsiyet"]
+# names(df1) <- "Deger"
+# 
+# df2 <- hobby["Fobi"]
+# names(df2) <- "Deger"
+# 
+# rbind_data2 <- rbind(df1, df2)
